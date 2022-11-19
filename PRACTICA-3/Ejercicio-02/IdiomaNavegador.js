e15e5e036d3f033427b179321cde86a31bdf019a
uo246355 <55742574+uo246355@users.noreply.github.com>
2022-11-19 15:51:10 +0100

    Delete PRACTICA-3 directory

--- PRACTICA-3/Ejercicio-02/IdiomaNavegador.js
+++
@@ -1,4 +0,0 @@
-// Informacion del navegador
-// Propiedades obsoletas appName, appVersion, platform 
-// ver https://developer.mozilla.org/en-US/docs/Web/API/Navigator
-document.write(infoNavegador.idioma);
