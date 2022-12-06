a291745111deec47b94f9c763021b535fc484ce1
uo246355 <55742574+uo246355@users.noreply.github.com>
2022-12-06 23:11:51 +0100

    ytyty

--- UO246355-practica3seccion2/Ejercicio-11/Tarea-6/Ejercicio11.js
+++ UO246355-practica3seccion2/Ejercicio-11/Tarea-6/Ejercicio11.js
@@ -113,5 +113,48 @@ class GeoLocalizacion {
               handleLocationError(false, infoWindow, mapaOviedo.getCenter()), mapaOviedo;
             }
     }
+    cargarDatos(){
+        console.log("llegamos");
+            $.ajax({
+                dataType: "json",
+                url: "https://api.openweathermap.org/data/2.5/weather?lat="+this.latitud+"&lon="+this.longitud+"&appid=fb96cae6f44d2c1c9bf5803657a29682",
+                method: 'GET',
+                success: function(datos){
+                        //$("pre").text(JSON.stringify(datos, null, 2)); //muestra el json en un elemento pre
+                    
+                        //Presentación de los datos contenidos en JSON
+                        console.log("llegue");
+                        var stringDatos = "<ul><li>Ciudad: " + datos.name + "</li>";
+                            stringDatos += "<li>País: " + datos.sys.country + "</li>";
+                            stringDatos += "<li>Latitud: " + datos.coord.lat + " grados</li>";
+                            stringDatos += "<li>Longitud: " + datos.coord.lon + " grados</li>";
+                            stringDatos += "<li>Temperatura: " + datos.main.temp + " grados Celsius</li>";
+                            stringDatos += "<li>Temperatura máxima: " + datos.main.temp_max + " grados Celsius</li>";
+                            stringDatos += "<li>Temperatura mínima: " + datos.main.temp_min + " grados Celsius</li>";
+                            stringDatos += "<li>Presión: " + datos.main.pressure + " milibares</li>";
+                            stringDatos += "<li>Humedad: " + datos.main.humidity + " %</li>";
+                            stringDatos += "<li>Amanece a las: " + new Date(datos.sys.sunrise *1000).toLocaleTimeString() + "</li>";
+                            stringDatos += "<li>Oscurece a las: " + new Date(datos.sys.sunset *1000).toLocaleTimeString() + "</li>";
+                            stringDatos += "<li>Dirección del viento: " + datos.wind.deg + " grados</li>";
+                            stringDatos += "<li>Velocidad del viento: " + datos.wind.speed + " metros/segundo</li>";
+                            stringDatos += "<li>Hora de la medida: " + new Date(datos.dt *1000).toLocaleTimeString() + "</li>";
+                            stringDatos += "<li>Fecha de la medida: " + new Date(datos.dt *1000).toLocaleDateString() + "</li>";
+                            stringDatos += "<li>Descripción: " + datos.weather[0].description + "</li>";
+                            stringDatos += "<li>Visibilidad: " + datos.visibility + " metros</li>";
+                            stringDatos += "<li>Nubosidad: " + datos.clouds.all + " %</li>";
+                            stringDatos += '<li> <img src="https://openweathermap.org/img/w/' + datos.weather[0].icon +'.png" alt="Icono de OpenWeather" /></li></ul>'
+                            console.log(stringDatos);
+                        $("footer").append(stringDatos);
+                    },
+                error:function(){
+                  $("h2").html("¡Tenemos problemas! No puedo obtener JSON de <a href='https://openweathermap.org'>OpenWeatherMap</a>"); 
+                  $("p").remove();
+                    }
+            });
+    }
+    mapaytiempo(){
+        this.mapaDinamico();
+        this.cargarDatos();
+    }
 }
 var geolocation = new GeoLocalizacion();
